f50bb624836871365dfef7b7316700cf5e78a285
Mikhail Kaminsky <kam.m.a@ya.ru>
2019-11-18 07:42:53 +0300

    Add questions to test serializer

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class QuestionSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :title, :question_type, :answer
 end
--- app/serializers/test_serializer.rb
+++ app/serializers/test_serializer.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 class TestSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :title
+  has_many :questions, serializer: QuestionSerializer
 end
