2bfade82fda47679282cd746333042311ae2fdaa
Mikhail Kaminsky <kam.m.a@ya.ru>
2020-04-30 17:01:08 +0300

    Add site to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /http:\/\/localhost:[0-9]{4}/
+    origins /http:\/\/localhost:[0-9]{4}/, 'https://quiz-app-reactttt.netlify.app'
 
     resource '*',
       headers: :any,
